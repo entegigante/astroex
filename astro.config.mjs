e535914aa8696622a29cc57d8ab965ad84642a49
Carlos Esparza Espinoza <carlos@exosfera.org>
2025-08-12 14:23:36 -0300

    Fix tailwind 2

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,22 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import { resolve } from 'path';
+import { fileURLToPath } from 'url';
+import { dirname, resolve } from 'path';
+
+// Resolve __dirname for ES modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://carlosesparza.github.io', // Reemplaza con tu URL de GitHub Pages
-  base: '/astroex', // Reemplaza con el nombre de tu repositorio
+  site: 'https://entegigante.github.io', // Tu URL de GitHub Pages
+  base: '/astroex', // El nombre de tu repositorio
   integrations: [tailwind()],
   vite: {
     resolve: {
       alias: {
-        '~': resolve(__dirname, 'src')
+        '~': resolve(__dirname, './src')
       }
     }
   }
